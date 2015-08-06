68dd88ececefb3a032b8d93d40b9e746b0687665
cabol <candres.bolanos@gmail.com>
2015-08-06 09:28:41 -0500

    Fixed elvis comments.

--- test/cowboy_swagger_test_utils.erl
+++ test/cowboy_swagger_test_utils.erl
@@ -15,7 +15,7 @@
 -spec all(atom()) -> [atom()].
 all(Module) ->
   ExcludedFuns = [module_info, init_per_suite, end_per_suite, group, all],
-  Exports = Module:module_info(exports),
+  Exports = apply(Module, module_info, [exports]),
   [F || {F, 1} <- Exports, not lists:member(F, ExcludedFuns)].
 
 -spec init_per_suite(config()) -> config().
